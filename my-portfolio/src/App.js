9af155f3c81dbaf72cc2cdb393e15a2b29bfcca9
Lawrence <156478905+a00294663@users.noreply.github.com>
2024-10-12 19:59:07 +0000

    component modified

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,4 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -13,12 +11,12 @@ function App() {
     <Router>
       <GlobalStyles />
       <Header />
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
